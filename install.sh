95be22d569467f68070fb29815b20b9cc139b7f7
mellic03 <mellic03@gmail.com>
2026-09-13 23:18:55 +1000

    woop

--- install.sh
+++ install.sh
@@ -9,13 +9,7 @@ if [[ "$SDK_VERSION" == "" ]]; then
     echo "  where VERSION is 165, 164, 163, ..., 100"
     exit 1
 fi
-SDK_FILENAME="steamworks_sdk_${SDK_VERSION}.zip"
+SDK_FILENAME="steamworks_sdk_$SDK_VERSION.zip"
 
-mkdir -p ${THIS_DIR}/download
-cd ${THIS_DIR}/download
-
-if [[ ! -f "${SDK_FILENAME}" ]]; then
-    wget --content-disposition "https://partner.steamgames.com/downloads/${SDK_FILENAME}"
-fi
-
+cd ${THIS_DIR}/dist
 unzip -o ${SDK_FILENAME} -d ${THIS_DIR}
